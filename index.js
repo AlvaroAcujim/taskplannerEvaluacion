6ca568992c66ee14a38f2131d5b17ff37c43e413
Álvaro <alvarorodrigoaj@gmail.com>
2025-04-21 15:19:21 +0200

    Needed comment to specify how to get token and idTable for baserow api

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ const showNotStarted = document.getElementById('showNotStarted');
 
 let task = [];
 let idTask = 0;
+//El token y el idtable se consigue dentro de baserow, primero hay que crear una base de datos -> click en los 3 puntitos (numero entre parentesis)
+//El token se obtiene en tu perfil -> mi configuración -> fichas de la base de datos (dar permisos). La base de datos tiene que tener:
+//Columnas: +Fecha -> fecha con hora, +Descripción -> texto largo, +estado -> selección única opciones: 1. Pendiente 2. Finalizado 3. No iniciada, 
+// +titulo ->Texto de una sola línea y por último fecha_creacion -> fecha con hora. Es importante tanto el orden como esta escrito en estos comentarios
 let token = '';
 let idTable = '';
 let lon;
